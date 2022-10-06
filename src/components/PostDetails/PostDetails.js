3b8514039f72d2ee85b5d8086cf8048d8d7bc9c6
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-10-07 05:26:36 +0600

    edit practice

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -3,7 +3,7 @@ import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const postDetails = useLoaderData()
-    const {id,tittle,body,userId} = postDetails
+    const {id,title,body,userId} = postDetails
     const navigate = useNavigate()
     const handlePostDetails = ()=>{
        navigate(`/friend/${userId}`)
@@ -11,7 +11,7 @@ const PostDetails = () => {
     return (
         <div>
             <h1>Post Details!!:{id}</h1>
-            <h2>Tilte:{tittle}</h2>
+            <h2>Tilte:{title}</h2>
             <p>{body}</p>
             <button onClick={handlePostDetails}>Get Author name</button>
 
